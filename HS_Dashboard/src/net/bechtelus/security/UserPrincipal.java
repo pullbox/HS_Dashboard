66d43a9b581c005a9f10efb53ae730a1b9559db9
Daniel Bechtel <daniel@bechtelus.net>
2017-05-01 17:22:12 -0400

    added Security basics
    RolePrincipal and UserPrincipal

---
+++ HS_Dashboard/src/net/bechtelus/security/UserPrincipal.java
@@ -0,0 +1,22 @@
+package net.bechtelus.security;
+
+import java.security.Principal;
+
+public class UserPrincipal implements Principal {
+
+	private String name;
+
+	public UserPrincipal(String name) {
+		super();
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+}
